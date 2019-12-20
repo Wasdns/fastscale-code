8c83ff3df50682f0ddb458c28e41de04bc1ce3db
ProfessorChen <mulThree@163.com>
2019-12-21 02:31:37 +0800

    add benchmark

--- spfa/dag.cpp
+++ spfa/dag.cpp
@@ -15,12 +15,10 @@ struct Edge
 		dis = _dis;
 	}
 };
-vector<Edge> g[MAXN]; //邻接表
 
+vector<Edge> g[MAXN]; //邻接表
 ll m; // 每层多少结点
 ll n;
-ll dis[MAXN], vis[MAXN], pre[MAXN];
-ll src, dst;
 
 
 
@@ -40,49 +38,11 @@ void dag()
 	}
 }
 
-void SPFA()
-{
-	queue<ll>  q;
-	for(ll i = 1; i <= n; i++)
-	{
-		dis[i] = INF;
-	}
-	memset(vis, 0, sizeof(vis));
-	q.push(src);
-	vis[src] = 1;
-	dis[src] = 0;
-	while(!q.empty())
-	{
-		ll u = q.front();
-		q.pop();
-		vis[u] = 0;
-		for(ll i = 0; i < g[u].size(); i++)
-		{
-			ll v = g[u][i].next;
-			if(dis[v] > dis[u] + g[u][i].dis)
-			{
-				dis[v] = dis[u] + g[u][i].dis;
-				pre[v] = u;
-				if(vis[v] == 0)
-				{
-					vis[v] = 1;
-					q.push(v);
-				}
-			}
-		}
-	}
-}
 
 
-int main(int argc, char *argv[])
+int main()
 {
-	for (int i = 1; i < argc; i++)
-	{
-		if (strcmp(argv[i], "-m") == 0)
-		{
-			m = atoi(argv[i + 1]);
-		}
-	}
+	scanf("%lld",&m);
 	n = M*m;
 	dag();
 	for(ll i = 1; i <= n; i++)
@@ -92,34 +52,6 @@ int main(int argc, char *argv[])
 			printf("%lld %lld %lld\n", i, g[i][j].next, g[i][j].dis);
 		}
 	}
-	src = 1;
-	dst = n;
-	while(1)
-	{
-		memset(vis, 0, sizeof(pre));
-		SPFA();
-		if(dis[dst] == INF)
-		{
-			break;
-		}	
-		printf("The shortest distance from %lld to %lld is %lld\n", src, dst, dis[dst]);
-		ll  l = pre[dst], r = dst;
-		printf("The shortest path from %d to %d is %d ", dst, src, r);
-		while(l != 0)
-		{
-			printf("-> %d ", l);
-			for(int i = 0; i < g[l].size(); i++)
-			{
-				if(g[l][i].next == r)
-				{
-					g[l][i].dis = INF;
-					break;
-				}
-			}
-			r = l;
-			l = pre[l];
-		}
-		printf("\n");
-	}
+	
 	return 0;
 }
